60ab2375a2e028ae1637ebc1290ff1231f0b27c5
brunobdias <brunobdias@live.com>
2020-03-03 02:44:17 +0000

    adjust ex11 and notes about

--- python/python_practice/ex11.py
+++ python/python_practice/ex11.py
@@ -1,11 +1,14 @@
-print "How old are you?"
+print "How old are you?",
 age = raw_input()
-print "How tall are you?"
+print "How tall are you?",
 height = raw_input()
 #height = height * 100
 #For this adjust on top we need convert the input to float number
-print "How much do you weight?"
+print "How much do you weight?",
 weight = raw_input()
 
 print "So, you're %r years old, %r cm tall and %r kg heavy." % (
     age, height, weight)
+
+#NOTE: Notice that we put a , (comma) at the end of each print line. This is so that
+#print doesn’t end the line with a new line character and go to the next line.
